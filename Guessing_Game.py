b51ed78c09e223f5498137c998aeaf2548f87ff1
rileymayes <rlmayes2@gmail.com>
2017-04-23 19:00:56 -0400

    Finished Guessing Game

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -7,7 +7,7 @@ Created on Sun Apr 23 17:06:25 2017
 import random
 
 def valid_number(s):
-    if s.isdigit() and 1 <= int(s) <= 3
+    if s.isdigit() and 1 <= int(s) <= 3:
         return True
     else:
         return False
@@ -15,23 +15,23 @@ def valid_number(s):
 def main():
     num = random.randint(1,3)
     number_guesses = 0
-    var = raw_input("Would you like to guess a number: ")
-    print "You entered: ", var, "Let's check!"
+    guess = raw_input("Would you like to guess a number: ")
+    print "You entered: ", guess, "Let's check!"
     while not False:
         if not valid_number(guess):
             guess = raw_input("That isn't a number. Please pick a number: ")
             continue
         else:
-            number_guesses = +=1
+            number_guesses +=1
             guess = int(guess)
-        
-        if var == num:
+            
+        if guess == num:
             print "You answered correctly!"
             print "It took", number_guesses, "to get the answer."
             break
-        elif var < num:
+        elif guess < num:
             print "Your guess is too low!"
-            var =  raw_input("Guess again: ")
-        elif var > num:
+            guess =  raw_input("Guess again: ")
+        else:
             print "Your guess is too high!"
-            var =raw_input("Guess again: ")
+            guess =raw_input("Guess again: ")
